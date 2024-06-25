5a948c50e648d8e633b0f845e788f6186b953dc2
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-06-26 00:35:45 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -17,7 +17,8 @@ API_HASH = "694b063e55c24287a3d30aed90191373"
 BOT_TOKEN = "7482584259:AAHbnDAq7nBXsC50if9rK0dOivlLG9XlvcQ"
 SESSION = "BQCMLuIAhzhCjSe5570KRaB_uThKmEJ13nMn4o3BCSdQPIGrkv_RyfrEG63Icb3VbM0SeNR5hQhGWFz-tEVu_6Rf4XLSQ02MOkx8VEE1zbkeOP7JonMk6P8tzTYwjIOosfRZoG2xA53PvlpCrjZKwpad2xhVZleTlYlv80Ocjgld65f5TRklL992eKgR1sm3Gy9TtJoqoFE_bhdrZTxfvIrCyTKDVCy0Gsicm60cvAvKA1zx5iwpuAMpZDot4HSRlvljlT10jhGF1q1yQgGUcvqsbFhaRuVQYIRlxwQDnuBFFnw07bz-gKjSDbEogBIpNkFM3MsZcWOZjk0Oz_KnhyuwZLfzzwAAAAGX-jAhAA"
 AUTH = 6844723233
-DB_CHANNEL = -1002010535930
+FORCESUB = "forcesubpavo4"
+
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
 userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
